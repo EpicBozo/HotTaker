9adf58531c93e8014ea3b8a8cbadb611ba6a702d
Ayman Ahsan <aymanahsan06@gmail.com>
2025-01-16 16:21:55 -0500

    formatted the basic stuff, time to branch out

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def signup(request):
+    print('signup')
+    return render(request, 'signup.html')
+
+def login(request):
+    print('login')
+    return render(request, 'login.html')
