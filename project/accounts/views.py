522f776256d59a2247161e564e19ab7c60d138a3
Ayman Ahsan <aymanahsan06@gmail.com>
2025-01-17 00:19:32 -0500

    ama hop off for now

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from accounts.forms import SignUpForm, LoginForm
 from accounts.models import Account
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.conf import settings
 
 # Create your views here.
 
@@ -8,9 +15,8 @@ def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            print("User created")
-            return render(request, 'accounts/login.html')
+            verify(form)
+            return redirect('login')
         else:
             print("Form is not valid")
             print(form.errors)
@@ -18,5 +24,35 @@ def signup(request):
     return render(request, 'accounts/signup.html')
 
 def login(request):
-    print('login')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            return redirect('home')
+        else:
+            print("Form is not valid")
+            print(form.errors)
+            return render(request, 'accounts/login.html', {'form': form})
     return render(request, 'accounts/login.html')
+
+def verify(form):
+    user = form.save(commit=False)
+    user.is_active = False
+    user.save()
+
+    token = default_token_generator.make_token(user)
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    
+    verification_link = f"{settings.DOMAIN}/accounts/verify/{uid}/{token}"
+    send_verification_email(user.email, verification_link)
+
+def send_verification_email(email, verification_link):
+    subject = 'Verify your email'
+    message = render_to_string('accounts/verify_email.html', {'link': verification_link})
+    from_email = settings.EMAIL_HOST_USER
+    to = email
+
+    send_mail(subject, message, from_email, [to])
+
+def verify_email(request, uidb64, token):
+
+    return render(request, 'accounts/verify_email.html')
--- project/project/settings.py
+++ project/project/settings.py
@@ -145,3 +145,16 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#email config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'noreply.bubbl@gmail.com'
+EMAIL_HOST_PASSWORD = 'tzrl hayq gbqs hxah'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+DOMAIN = 'http://127.0.0.1:8000'
