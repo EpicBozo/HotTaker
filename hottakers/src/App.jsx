e92d79e8f2f02384e43da3dbc7a9e1f4b5df3bab
Ayman Ahsan <aymanahsan06@gmail.com>
2025-01-20 17:00:56 -0500

    bro what is going on

--- hottakers/src/App.jsx
+++ hottakers/src/App.jsx
@@ -1,33 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./Header.jsx";
 import CreatePost from "./CreatePost.jsx";
 import Footer from "./Footer.jsx";
-import Login from './components/accounts/login';
-import SignUp from './components/accounts/signup';
-import VerificationSuccess from './components/accounts/VerificationSuccess';
-import axios from 'axios';
-
-// Set base URL for all axios requests
-axios.defaults.baseURL = 'http://localhost:8000';
-axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.withCredentials = true;
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<CreatePost />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/verify/:uid/:token" element={<VerificationSuccess />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <>
+      <Header />
+      <CreatePost />
+      <Footer />
+    </>
   );
 }
 
